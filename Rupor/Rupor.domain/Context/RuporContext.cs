8f185744eac65ae47746ff9515cb04a2596128d3
Михаил <milkyway211092@gmail.com>
2019-01-04 12:31:19 +0300

    Add EF context

---
+++ Rupor/Rupor.domain/Context/RuporContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Rupor.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rupor.domain.Context
+{
+    public class RuporDbContext:IdentityDbContext<UserEntity>
+    {
+        public RuporDbContext()
+        {
+
+        }
+
+    }
+}
